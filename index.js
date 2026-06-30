68d0a4ff8f1249cc7f9eaf02fc38de17e0743d23
Fatima Ndiaye <faatimandiaye@gmail.com>
2026-06-30 22:44:48 +0000

    c

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const connectBD = require("./config/db");
 const userRoute = require('./routes/user.route');
 const questionRoute = require('./routes/question.route');
+const reponseRoute = require('./routes/reponse.route');
 
 dotenv.config();
 const app = express();
@@ -11,18 +12,20 @@ connectBD();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://frontend-node.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
+app.use('/api/auth', userRoute);
+app.use('/api/questions', questionRoute);
+app.use('/api/questions/:questionId/reponses', reponseRoute);
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur mon serveur');
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`serveur démarré sur http://localhost:${PORT}`);
-});
-
-app.use('/api/auth', userRoute);
-app.use('/api/questions', questionRoute);
-app.get('/', (req, res) => {
-    res.send('Bienvenue sur mon serveur');
 });
